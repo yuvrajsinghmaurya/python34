7fc6036b92caa9309ff6c4e8edd76217304517a4
yuvraj singh <127769740+yuvrajsinghmaurya@users.noreply.github.com>
2023-03-13 21:35:00 +0530

    Add files via upload

---
+++ 02 practice 01  01.py
@@ -0,0 +1,6 @@
+# write a prog to print geeta shlok in python
+print('''The Vedas deal with the three modes of material nature, O Arjun.
+ Rise above the three modes to a state of pure spiritual consciousness. 
+ Freeing yourself from dualities, eternally fixed in Truth, and without 
+ concern for material gain and safety, be situated in the self.''')
+ # here we want to print multiple line so in place of "" we use ''' colume
